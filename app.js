6ad1ac5dab0a07a10fcbf6b7e8648a9ec4ee6153
Andrew Kochetygov <flatdragon@yandex.ru>
2020-12-14 01:26:33 +0700

    fix

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-export default (express, bodyParser, fs, crypto, http, mongodb, path, cors, puppeteer) => {
+export default (express, puppeteer) => {
+    
     const app = express();
+    const author = 'itmo282167'
 
-    app.use(bodyParser.json());
-    app.use(express.urlencoded());
     app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+        res.setHeader('Content-Type', 'text/plain')
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
         next()
     });
 
-    app.use(cors());
-    app.options('*', cors());
+    app.get('/login/', (req, res) => {
+        res.send(author)
+    })
 
-    app 
-        .get('/test/', async (req, res) => {
-            res.setHeader('content-type', 'text/plain');
-            res.send("0.8862481722945399");
+    app.get('/test/', async (req, res) => {
+        const { URL } = req.query
+
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.goto(URL)
+
+        const value = await page.evaluate(async () => {
+            const button = document.getElementById('bt')
+            const input = document.getElementById('inp')
+
+            button.click()
+
+            return input.value
         })
-        .get('/login/', (req, res) => res.send('itmo282167'))
-        .all('*', (req, res) => {
-            res.send('itmo282167');
-        });
+
+        res.send(value)
+    })
+
+    app.all('*', (req, res) => {
+        res.send(author)
+    })
 
 
     return app;
--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongodb from 'mongodb';
-import cors from 'cors';
-import puppeteer from 'puppeteer';
+import express from 'express'
+import puppeteer from 'puppeteer'
 
-import path from 'path';
-import fs from 'fs';
-import crypto from 'crypto';
-import http from 'http';
+import createApp from './app.js'
 
-import appSrc from './app.js';
+const app = createApp(express, puppeteer)
 
-const app = appSrc(express, bodyParser, fs, crypto, http, mongodb, path, cors, puppeteer);
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log('Server is up!');
-});
+app.listen(process.env.PORT || 3000)
